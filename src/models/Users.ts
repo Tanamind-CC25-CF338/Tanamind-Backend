1a4098d239a983db97f5fd5e11b2701e4a2d8ae1
muhammadfauzanis <mfauzann982@gmail.com>
2025-05-06 19:58:10 +0700

    create user model

---
+++ src/models/Users.ts
@@ -0,0 +1,68 @@
+import prisma from '../db';
+
+export const findUserByEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  return user;
+};
+
+export const findUserById = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  return user;
+};
+
+export const findResetPasswordTokenUser = async (
+  resetPasswordToken: string
+) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      resetPasswordToken: resetPasswordToken,
+    },
+  });
+
+  return user;
+};
+
+export const createUser = async (userData: any) => {
+  const user = await prisma.user.create({ data: userData });
+
+  return user;
+};
+
+export const updateResetPasswordToken = async (
+  userId: string,
+  resetPasswordToken: string | null,
+  resetPasswordTokenExpired: Date | null
+) => {
+  const user = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      resetPasswordToken: resetPasswordToken,
+      resetPasswordTokenExpired: resetPasswordTokenExpired,
+    },
+  });
+
+  return user;
+};
+
+export const updatePassword = async (userId: string, newPassword: string) => {
+  const user = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      password: newPassword,
+    },
+  });
+};
