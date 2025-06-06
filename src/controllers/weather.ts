843d80d49e51fd6c7ea0acbfcfca103bd7ee3738
muhammadfauzanis <mfauzann982@gmail.com>
2025-06-06 17:59:17 +0700

    change weather controller

--- src/controllers/weather.ts
+++ src/controllers/weather.ts
@@ -1,16 +1,36 @@
+// src/controllers/weather.ts
 import { Request, Response } from 'express';
-import { getWeatherByCoords } from '../services/weatherService';
+import { getWeatherForecast } from '../services/weatherService';
 import response from '../response';
 
-export const getWeather = async (req: Request, res: Response) => {
+export const fetchWeatherForecast = async (req: Request, res: Response) => {
   try {
     const { lat, lon } = req.query;
     if (!lat || !lon) {
-      return response(400, null, 'Latitude dan longitude wajib diisi', res);
+      return response(400, null, 'Latitude dan Longitude diperlukan', res);
     }
 
-    const weather = await getWeatherByCoords(Number(lat), Number(lon));
-    return response(200, weather, 'Berhasil mengambil data cuaca', res);
+    const data = await getWeatherForecast(Number(lat), Number(lon));
+
+    const grouped = data.list.reduce((acc: any[], item: any) => {
+      const date = item.dt_txt.split(' ')[0];
+      if (!acc.find((i) => i.date === date)) {
+        acc.push({
+          date,
+          temp: item.main.temp,
+          weather: item.weather[0].main,
+          icon: item.weather[0].icon,
+        });
+      }
+      return acc;
+    }, []);
+
+    return response(
+      200,
+      grouped.slice(0, 4),
+      'Berhasil mengambil data cuaca',
+      res
+    );
   } catch (err) {
     console.error('❌ Weather error:', err);
     return response(500, null, 'Gagal mengambil data cuaca', res);
--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,25 +1,19 @@
+// src/services/weatherService.ts
 import axios from 'axios';
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY!;
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const getWeatherByCoords = async (lat: number, lon: number) => {
-  const res = await axios.get(BASE_URL, {
+export const getWeatherForecast = async (lat: number, lon: number) => {
+  const res = await axios.get(`${BASE_URL}/forecast`, {
     params: {
       lat,
       lon,
-      appid: WEATHER_API_KEY,
       units: 'metric',
+      cnt: 32, // forecast 3 jam * 32 = 4 hari
+      appid: WEATHER_API_KEY,
     },
   });
 
-  const { weather, main, wind } = res.data;
-
-  return {
-    temperature: main.temp,
-    humidity: main.humidity,
-    condition: weather[0].main,
-    wind_speed: wind.speed,
-    description: weather[0].description,
-  };
+  return res.data;
 };
